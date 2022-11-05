0f24a3fb3a16ea2f7b1c3067486559afbc9b28d5
Anton Gusev <gans-AD@yandex.ru>
2022-11-05 13:29:05 +0300

    feat: компонент Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Footer from "../Footer/Footer.js"
+import Promo from "../Promo/Promo"
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="page">
+    <Promo></Promo>
+    <Footer></Footer>
+  </div>;
 }
 
 export default App;
