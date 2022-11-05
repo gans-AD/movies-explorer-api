c20ad8a5e5740fc09fc30e6505689d53c9c663b6
Anton Gusev <gans-AD@yandex.ru>
2022-11-05 22:59:20 +0300

    feat: компонент NavTab

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Footer from "../Footer/Footer.js"
 import Promo from "../Promo/Promo"
+import NavTab from "../NavTab/NavTab";
 
 function App() {
   return <div className="page">
     <Promo></Promo>
+    <NavTab></NavTab>
     <Footer></Footer>
+    
   </div>;
 }
 
