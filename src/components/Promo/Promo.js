6ce89cac22565fc7fe62b856b29ac4d7168790e6
Anton Gusev <gans-AD@yandex.ru>
2022-11-05 15:41:20 +0300

    feat: компонент Promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -3,6 +3,7 @@ import "./Promo.css";
 function Promo() {
   return (
     <section className="promo">
+      <div className="promo__logo"></div>
       <h1 className="promo__title">
         Учебный проект студента факультета Веб-разработки.
       </h1>
