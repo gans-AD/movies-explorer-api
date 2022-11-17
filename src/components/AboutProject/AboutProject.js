3bf3078ac5ad6b4d2143e21b48cbf323a966b671
Anton Gusev <gans-AD@yandex.ru>
2022-11-17 06:43:53 +0300

    feat: компоненты Description и AboutProject

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,41 @@
+import "./AboutProject.css";
+
+function AboutProject() {
+  return (
+    <section className="about-project">
+      <article className="two-columns">
+        <div className="two-columns__column">
+          <h3 className="two-columns__title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="two-columns__text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </div>
+
+        <div className="two-columns__column">
+          <h3 className="two-columns__title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="two-columns__text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+        
+      </article><div className="timeline">
+          <div>
+            <div className="timeline__duration timeline__duration_green">1 неделя</div>
+            <div className="timeline__description">Back-end</div>
+          </div>
+          <div>
+            <div className="timeline__duration">4 недели</div>
+            <div className="timeline__description">Front-end</div>
+          </div>
+        </div>
+    </section>
+  );
+}
+
+export default AboutProject;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,18 @@ import "./App.css";
 import Footer from "../Footer/Footer.js"
 import Promo from "../Promo/Promo"
 import NavTab from "../NavTab/NavTab";
+import AboutProject from "../AboutProject/AboutProject";
+import Description from "../Description/Description";
 
 function App() {
   return <div className="page">
-    <Promo></Promo>
-    <NavTab></NavTab>
-    <Footer></Footer>
+    <Promo/>
+    <NavTab/>
+    <Description title="О проекте">
+      <AboutProject />
+    </Description>
+    <Description title="Технологии"></Description>
+    <Footer/>
     
   </div>;
 }
