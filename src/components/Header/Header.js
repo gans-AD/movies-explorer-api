80bf68ceb61d75005a8cc37b19674ef10663d99a
Anton Gusev <gans-AD@yandex.ru>
2023-01-23 15:03:33 +0000

    компонент header для страницы Landing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ function Header() {
   return (
     <header className="header">
       <div className="header__logo"></div>
-      <button type="button" className="header__reg-button"></button>
-      <button type="button" className="header__login-button"></button>
+      <button type="button" className="header__reg-button">Регистрация</button>
+      <button type="button" className="header__login-button">Войти</button>
     </header>
   );
 }
