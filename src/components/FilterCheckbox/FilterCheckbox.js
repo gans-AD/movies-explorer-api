cb63118f4a1edbbff70e920439d13e7ff888ddd3
Anton Gusev <gans-AD@yandex.ru>
2023-02-09 01:08:28 +0300

    feat: FilterChecbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,13 +1,24 @@
+import React from "react";
 import "./FilterCheckbox.css";
 
 function FilterCheckbox() {
+  const [checked, setChecked] = React.useState(false);
+
+  function handleChangeCheckbox() {
+    setChecked(!checked);
+  }
+
   return (
-    <div className="filter">
-      <label className="switch">
-        <input type="checkbox" className="filter__checkbox"></input>
-      </label>
-      <span className="filter__name">Короткометражки</span>
-    </div>
+    <label className="switch">
+      <input
+        type="checkbox"
+        className="switch__checkbox"
+        checked={checked}
+        onChange={handleChangeCheckbox}
+      ></input>
+      <div className="switch__slider"></div>
+      <span className="switch__name">Короткометражки</span>
+    </label>
   );
 }
 
