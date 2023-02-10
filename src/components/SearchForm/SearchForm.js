b657d11b1d23397b3ec445b9b9214b6222aa4395
Anton Gusev <gans-AD@yandex.ru>
2023-02-11 02:13:01 +0300

    feat: адаптивность

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,28 @@
+import { useWindowWidth } from "@react-hook/window-size";
 import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 import "./SearchForm.css";
 
 function SearchForm() {
+  const windowWidth = useWindowWidth();
+  const mobileDisplay = windowWidth <= 685;
+
   return (
     <section className="search-form">
       <form name="searchMovies" className="search">
         <div className="search__input-wrapper">
           <div className="search__icon"></div>
-          <input name="movies" type="search" placeholder="Фильм" className="search__input"></input>
-          <button type="button" className="search__button">Найти</button>
-          <FilterCheckbox />
+          <input
+            name="movies"
+            type="search"
+            placeholder="Фильм"
+            className="search__input"
+          ></input>
+          <button type="button" className="search__button">
+            Найти
+          </button>
+          {!mobileDisplay && <FilterCheckbox />}
         </div>
+        {mobileDisplay && <FilterCheckbox />}
       </form>
     </section>
   );
